30aa0916bfb5aa00e93af21ccb476f8e33c2c3de
Lozarth <46830521+Lozarth@users.noreply.github.com>
2023-08-06 20:45:13 -0400

    Update autoconfirm.js

--- autoconfirm.js
+++ autoconfirm.js
@@ -7,6 +7,7 @@
 // @match        https://www.omegle.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=omegle.com
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function() {
